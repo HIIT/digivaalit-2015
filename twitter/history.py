f17fba298dab4bcb0a5fe323e7707b4bcb60440a
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-03-24 20:20:11 -0700

    Iterate over dates

--- twitter/history.py
+++ twitter/history.py
@@ -5,12 +5,15 @@
 import requests
 import bs4
 import re
+import datetime
 
 match_id = '.*/([0-9]+)'
 
 def _collect_ids( username , date ):
 
-    url = 'https://mobile.twitter.com/search?src=typd&q=from%3AJyrkikasvi%20since%3A2011-01-01%20until%3A2011-04-01'
+    date2 = date + datetime.timedelta( 1 )
+
+    url = 'https://mobile.twitter.com/search?src=typd&q=from%3A' + username + '%20since%3A' + date.isoformat() + '%20until%3A' + date2.isoformat()
 
     r = requests.get( url )
 
@@ -22,13 +25,32 @@ def _collect_ids( username , date ):
 
         if isinstance( tweet , bs4.Tag ):
 
-        ## get tweet ID
+            ## get tweet ID
             if 'href' in tweet.attrs:
                 href = tweet['href']
                 href = re.search( match_id, href )
                 ret.append( (int)( href.group(1) ) )
 
+            ## TODO: check if count > 19; then need to parse more
+
     return ret
 
+def collect_tweets( username, since, until ):
+
+    date = since
+
+    tweets = []
+
+    while date <= until:
+
+        tweets += _collect_ids( username, date )
+        date = date + datetime.timedelta( 1 )
+
+        print date
+
+    return tweets
+
+
+
 if __name__ == '__main__':
-    print _collect_ids( 'matnel', 'x' )
+    print len( collect_tweets( 'Jyrkikasvi', datetime.date( 2011, 1, 1 ), datetime.date( 2011, 1, 31 ) ) )
