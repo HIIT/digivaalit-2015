7b9d8285153e16d325e28681342aa2aee6f57fb8
Matti Nelimarkka <mnelimar@xs.hiit.fi>
2015-04-13 15:27:44 +0300

    We can pull several IDs at the same time

--- twitter/common.py
+++ twitter/common.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+import math
 
 from secrets import *
 
@@ -10,13 +11,23 @@ twitter = tweepy.API(auth)
 def tweets( ids ):
 
     ret = []
-    for tweet in ids:
+    
+    ## the API can take 100 per time
+    count = len( ids ) / 100
+    count = int( math.ceil( count ) )
+
+    for i in range( 0, count + 1 ):
+
+        _ids = ids[ i * 100 : ( i + 1 ) * 100 ]
+        _ids = ','.join( _ids )
+    
         t = None
         while not t:
             try:
-                t = twitter.get_status( tweet )._json
+                t = twitter._statuses_lookup( _ids )
+                t = map( lambda x: x._json , t )
             except tweepy.error.TweepError:
                 time.sleep( 60 * 5 ) ## wait for ratelimit to come back
-        ret.append( t )
+        ret += t
 
     return ret
