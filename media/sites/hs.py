1f130b995ec2e4de495cadbca51176659f63df79
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-04-21 22:31:31 +0300

    Revert previous changes from commit 76750c94663c6482af7a8aee47f5ef6522d59f3a

--- media/sites/hs.py
+++ media/sites/hs.py
@@ -12,9 +12,9 @@ def nouda( url , out ):
 	teksti = soup.find_all( id="article-text-content" )
 	for e in teksti[0]:
 		if isinstance( e, bs4.element.Tag):
-			if not e.get('class'):
-				out.write( repr( e.string ) )
+			if not e.get('class') and e.string:
+				out.write( e.string.encode('utf8') + ' ' )
 
 if __name__ == '__main__':
-	
-	nouda("http://www.hs.fi/ulkomaat/a1417488621135", file('hs.txt', 'w'))
+
+	nouda("http://www.hs.fi/paakirjoitukset/a1428030701507", file('hs.txt', 'w'))
