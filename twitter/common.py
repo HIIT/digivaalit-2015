14696235d67d2fa78514c79be6f40b135c04c3a4
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-06-04 11:01:48 +0300

    Fix rate limit code

--- twitter/common.py
+++ twitter/common.py
@@ -11,23 +11,27 @@ twitter = tweepy.API(auth)
 def tweets( ids ):
 
     ret = []
-    
+
     ## the API can take 100 per time
-    count = len( ids ) / 100
+    count = len( ids ) / 100.0
     count = int( math.ceil( count ) )
 
-    for i in range( 0, count + 1 ):
+    for i in range( 0, count ):
 
         _ids = ids[ i * 100 : ( i + 1 ) * 100 ]
+
+        print i, count, len( _ids )
+
         _ids = ','.join( _ids )
-    
+
+
         t = None
         while not t:
             try:
                 t = twitter._statuses_lookup( _ids )
                 t = map( lambda x: x._json , t )
             except tweepy.error.TweepError:
-                time.sleep( 60 * 5 ) ## wait for ratelimit to come back
+                time.sleep( 60 * 15 ) ## wait for ratelimit to come back
         ret += t
 
     return ret
