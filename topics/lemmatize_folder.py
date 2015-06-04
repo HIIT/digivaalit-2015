6cf06973bbd0983c1c1a1acbe6a165159f769fbf
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-06-04 16:44:02 +0300

    Use Python instead of shell script

---
+++ topics/lemmatize_folder.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import re
+import subprocess
+
+def lemmatize( text ):
+
+    text = text.encode('utf8')
+    text = re.sub( '[\.,?!:;]' , '' , text )
+
+    out = subprocess.check_output( 'module load finnish-process; echo "' + text + '" | finnish-process', shell = True)
+
+    lemma = ''
+    for line in out.split('\n'):
+        line = line.strip()
+        line = line.split('\t')
+
+        if len( line ) >= 2:
+            lemma += line[1] + ' '
+
+    return lemma
+
+## folder usecase
+path = sys.argv[1]
+for file in os.listdir( path ):
+
+    text = open( path + file )
+    text = text.readlines()
+    text = map( lambda x: x.strip(), text )
+    text = ' '.join( text )
+
+    lemma = lemmatize( text )
+
+    fo = open( path + file + '.lemma' )
+    fo.write( lemma )
+    fo.close()
