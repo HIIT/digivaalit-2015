03da69b8772289d7f759094102b8df23827d72d0
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-06-15 23:52:17 +0300

    Update implementation of topicmodel scripts

--- topics/topics.r
+++ topics/topics.r
@@ -22,42 +22,31 @@ create_dtm <- function( path ) {
 
 }
 
-check_number_of_topics <- function( dtm , maxk = 250 ) {
+create_model <- function( dtm, k ) {
+
+   library(topicmodels)
+
+   burnin = 1000
+   iter = 1000
+   keep = 50
+
+   model <- LDA( dtm , k = k, method = "Gibbs", control =  list(burnin = burnin, iter = iter, keep = keep) )
+
+   return model
+
+}
+
+check_fittness <- function( dtm , k ) {
 
   library(topicmodels)
   library(Rmpfr)
 
-  burnin = 1000
-  iter = 1000
-  keep = 50
+  model <- create_model( dtm , k )
+  ll <- model@logLiks[ -c(1:(burnin/keep)) ]
 
-  result <- data.frame( 'k' = integer(0), 'll' = integer(0) )
+  precision = 2000L llMed <- median( ll )
+  ll = as.double( llMed - log( mean( exp( -mpfr(ll , prec = precision) + llMed ) ) ) )
 
-  ## for memory restrictions, lets run this in separate sets
-  for( i in 0: round( maxk / 10 ) ) {
-
-    sequ <- seq( i * 10 + 1 , ( i + 1) * 10  )
-    fitted_many <- lapply( sequ, function(k) {
-  	   print(k);
-  	    LDA( dtm.new , k = k, method = "Gibbs", control = list(burnin = burnin, iter = iter, keep = keep) )
-    } )
-    logLiks_many <- lapply(fitted_many, function(L)  L@logLiks[-c(1:(burnin/keep))] )
-
-    harmonicMean <- function(logLikelihoods, precision=2000L) {
-      llMed <- median(logLikelihoods)
-      as.double(llMed - log(mean(exp(-mpfr(logLikelihoods, prec = precision) + llMed))))
-    }
-
-    hm_many <- sapply(logLiks_many, function(h) harmonicMean(h))
-
-
-    k = sequ[which.max(hm_many)]
-    ll = max( hm_many )
-
-    result <- rbind( result, c( k , ll) ) )
-
-  }
-
-  return result;
+  return ll
 
 }
