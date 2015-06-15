a8efcdef31530225d76f07d3ec96ceb53b6e505a
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-06-16 00:10:00 +0300

    Add visualization script for ldavis

--- topics/topics.r
+++ topics/topics.r
@@ -49,3 +49,37 @@ check_fitness <- function( dtm , k ) {
   return( ll )
 
 }
+
+
+## from http://www.r-bloggers.com/a-link-between-topicmodels-lda-and-ldavis/
+
+visualize_topicmodel <- function(fitted, corpus, doc_term){
+    # Required packages
+    library(topicmodels)
+    library(dplyr)
+    library(stringi)
+    library(tm)
+    library(LDAvis)
+ 
+    # Find required quantities
+    phi <- posterior(fitted)$terms %>% as.matrix
+    theta <- posterior(fitted)$topics %>% as.matrix
+    vocab <- colnames(phi)
+    doc_length <- vector()
+    for (i in 1:length(corpus)) {
+        temp <- paste(corpus[[i]]$content, collapse = ' ')
+        doc_length <- c(doc_length, stri_count(temp, regex = '\\S+'))
+    }
+    temp_frequency <- inspect(doc_term)
+    freq_matrix <- data.frame(ST = colnames(temp_frequency),
+                              Freq = colSums(temp_frequency))
+    rm(temp_frequency)
+ 
+    # Convert to json
+    json_lda <- LDAvis::createJSON(phi = phi, theta = theta,
+                            vocab = vocab,
+                            doc.length = doc_length,
+                            term.frequency = freq_matrix$Freq)
+ 
+    return(json_lda)
+}
