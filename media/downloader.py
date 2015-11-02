af986a8d3448fcde59fdedc355d2d87f4885d65f
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-02 15:24:01 +0200

    Work with generic filename

--- media/downloader.py
+++ media/downloader.py
@@ -6,22 +6,20 @@ urlpat = r'((http[s]?):\/\/)?(\w+\.)*(?P<domain>\w+)\.(\w+)(\/.*)?'
 
 outf = './media/'
 
-def download( link, time ):
+def download( link, filename ):
 
     link = link.strip()
 
 
     try:
-
-        ## link format: id, url
-        out = open( outf + str( hash( link ) ) + '_' + time + '.txt', 'w' )
-
         ## try to dynamically load the correct script using the domain name
         loader = re.match( urlpat , link ).group('domain')
 
         loader = structures = __import__( 'sites.' + loader, fromlist = [ loader ] )
 
         ## load the current story
+
+        out = open( outf + filename + '.txt', 'w' )
         loader.nouda( link, out )
         out.close()
 
