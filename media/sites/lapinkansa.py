ea5bfe240cc349144e089f606534726863f2c21b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-03 10:41:02 +0200

    Fix to new page layout

--- media/sites/lapinkansa.py
+++ media/sites/lapinkansa.py
@@ -8,7 +8,7 @@ def nouda( url , out ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text )
 
-	teksti = soup.find_all( class_ ='post-meta' )
+	teksti = soup.find_all( class_ = 'news-excerpt' )
 
 	for string in teksti[0].stripped_strings:
 	        out.write( string.encode('utf8') + ' ' )
