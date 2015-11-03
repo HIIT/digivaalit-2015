294f15f5ab3ed519b8b17d585e55dc62b7734b10
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-03 10:58:20 +0200

    Initial commit of kainuunsanomat

---
+++ media/sites/kainuunsanomat.py
@@ -0,0 +1,19 @@
+import requests
+
+from bs4 import BeautifulSoup
+import bs4
+
+def nouda( url , out ):
+
+	r = requests.get( url )
+	r.encoding = 'UTF-8'
+	soup = BeautifulSoup( r.text )
+
+	teksti = soup.find_all( class_='Teksti' )
+
+	for string in teksti[0].stripped_strings:
+	        out.write( string.encode('utf8') + ' ' )
+
+if __name__ == '__main__':
+
+	nouda("http://www.kainuunsanomat.fi/kainuun-sanomat/kotimaa/lipponen-moitti-sipilan-puheita-ministerien-vahentamisesta/", file('kainuunsanomat.txt', 'w'))
