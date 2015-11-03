a23080b663ccbdadcb157f617a956463c7d531fc
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-03 10:21:23 +0200

    Paivitetty uuteen versioon

--- media/sites/iltasanomat.py
+++ media/sites/iltasanomat.py
@@ -8,7 +8,10 @@ def nouda( url , out ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text )
 
-	teksti = soup.find_all( id="article-text" )
+	teksti = soup.find_all( class_= "content-inner" )
+	teksti[0].find( class_ = 'side-ads' ).decompose()
+
+	map( lambda x: x.extract() , teksti[0].find_all('script')  )
 
 	for string in teksti[0].stripped_strings:
 	        out.write( string.encode('utf8') + ' ' )
