c2ef5be0ab4b27c3940542545d68e1fdf1fefa31
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-01-10 16:03:40 +0200

    Cleaning YLE

--- vaalikone/yle_jsonize.py
+++ vaalikone/yle_jsonize.py
@@ -3,7 +3,31 @@
 import csv
 import json
 
+import re
+
 import sys
+
+def _reclean( candidate, field, cleaners, invalids ):
+
+    if candidate[ field ]:
+
+        for cleaner in cleaners:
+            candidate[ field ] = cleaner.sub( '' , candidate[ field ] )
+
+        for invalid in invalids:
+            if invalid in candidate[ field ]:
+                candidate[ field ] = None
+                return
+
+        if candidate[ field ] == '':
+            candidate[ field ] = None
+
+
+_clean_twitter = re.compile('((http)?[s]?(://)?(www.)?twitter(.com)?/|@)' , re.IGNORECASE )
+_clean_facebook = re.compile('(http)?[s]?(://)?(www.)?facebook.com/(pages/)?' , re.IGNORECASE )
+_clean_slash = re.compile('/')
+_clean_refs = re.compile('\?(f)?ref=[a-z]*')
+
 
 data = []
 
@@ -14,6 +38,7 @@ for row in csv.DictReader( open( sys.argv[1] , 'r' ) , delimiter = ';'  ):
     candidate['name'] = row['etunimi'] + ' ' + row['sukunimi']
     candidate['age'] = row['ikä']
     candidate['district'] = row['vaalipiiri']
+    candidate['number'] = row['ehdokasnumero']
 
     if row['sukupuoli']  == 'F':
         candidate['sex'] = 'FEMALE'
@@ -23,6 +48,9 @@ for row in csv.DictReader( open( sys.argv[1] , 'r' ) , delimiter = ';'  ):
     candidate['www'] = row['Kotisivun osoite:']
     candidate['facebook'] = row['Facebook-profiilin osoite:']
     candidate['twitter'] = row['Twitter-profiilin osoite:']
+
+
+    _reclean( candidate, 'twitter' , [ _clean_twitter , _clean_slash ], ['www.', 'http'] )
 
     data.append( candidate )
 
