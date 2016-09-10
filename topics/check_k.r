c0ae89a11d69bc1a1d329a29f17b927beabd2af7
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-09-10 20:36:08 +0300

    Make script work with several folders

--- topics/check_k.r
+++ topics/check_k.r
@@ -2,15 +2,19 @@ source('topics.r')
 
 df = data.frame( k = integer(), ll =integer() )
 
-path <- commandArgs(trailingOnly=TRUE)[0]
+for( path in commandArgs(trailingOnly=TRUE) ) {
 
-for( f in list.files(path) ){
-	load(f)
-	k <- model@k
-	ll <- check_fitness_model( model )
-	row = c(k, ll)
-	df[ nrow(df)+1,] <- row
+  for( f in list.files(path) ){
+  	load( paste(path, f, sep = '') )
+  	k <- model@k
+  	ll <- check_fitness_model( model )
+  	row = c(k, ll)
+  	df[ nrow(df)+1,] <- row
+  }
+
+  print("Examinging", path )
+  print("Best fit log likelihood", which.max( df$ll ) )
+  print("Best fit k", df$k[ which.max( df$ll ) ] )
+  print("") ## Empty line
+
 }
-
-print("Best fit log likelihood", which.max( df$ll ) )
-print("Best fit k", df$k[ which.max( df$ll ) ] )
