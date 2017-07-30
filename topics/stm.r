fc199548a75e68e934f99eb1fbe943b56f6b788b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-07-30 21:23:46 +0300

    Fix all bugs to make running

--- topics/stm.r
+++ topics/stm.r
@@ -2,13 +2,13 @@ create_model <- function( dtm, k ) {
 
   library(stm)
 
-  out <- readCorpus( dtm, type = "dtm" )
+  out <- readCorpus( dtm, type = "slam" )
 
   documents <- out$documents
   vocab <- out$vocab
 
-  topic <- stm(documents, vocab, init.type = "Spectral", max.em.its = 50)
+  topic <- stm(documents, vocab, K = k, init.type = "Spectral", max.em.its = 50)
 
-  return topic;
+  return( topic )
 
 }
